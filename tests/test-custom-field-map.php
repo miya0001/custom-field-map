1860d7c7666e3c31f7673093547ecff29b57a2fb
miya0001 <miya@wpist.me>
2017-08-08 01:42:23 +0900

    add some tests

--- tests/test-custom-field-map.php
+++ tests/test-custom-field-map.php
@@ -25,16 +25,53 @@ class Custom_Field_Map_Tests extends WP_UnitTestCase
 	 * @runInSeparateProcess
 	 * @preserveGlobalState disabled
 	 */
-	function test_enqueue_fires_on_page() {
-		$map = new Miya\WP\Custom_Field\Map( 'map', 'Map' );
-		$map->add( array( 'page', 'post' ) );
+	function test_enqueue_fires_on_page()
+	{
+		register_post_type( 'my-post-type' );
 
-		convert_to_screen( 'page' );
-		set_current_screen( 'post-new.php' );
+		$map = new Miya\WP\Custom_Field\Map( 'map', 'Map' );
+		$map->add( array( 'page', 'my-post-type' ) );
+
+		$GLOBALS['current_screen'] = convert_to_screen( 'my-post-type' );
 		do_action( 'admin_enqueue_scripts', 'post-new.php' );
 		$this->assertTrue( wp_style_is( 'leaflet' ) );
 		$this->assertTrue( wp_script_is( 'riot' ) );
 		$this->assertTrue( wp_script_is( 'leaflet' ) );
 		$this->assertTrue( wp_script_is( 'app' ) );
+	}
+
+	/**
+	 * A test `form()` should be called.
+	 */
+	function test_form_should_be_called()
+	{
+		global $wp_meta_boxes;
+
+		$test = new Miya\WP\Custom_Field\Map( 'hello', 'Hello' );
+		$test->add( 'my-post-type' );
+
+		do_action( 'add_meta_boxes' );
+
+		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+
+		update_post_meta( $post_id, '_hello-lat', '1111' );
+		update_post_meta( $post_id, '_hello-lng', '2222' );
+
+		$GLOBALS['post'] = get_post( $post_id );
+
+		ob_start();
+		$metaboxes = $wp_meta_boxes['my-post-type']['advanced']['default'];
+		call_user_func(
+			array( $metaboxes['hello']['callback'][0], $metaboxes['hello']['callback'][1] ),
+			get_post( $post_id ),
+			array()
+		);
+		$res = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertRegExp( '#Latitude: <input id="hello-lat" type="text"#', $res );
+		$this->assertRegExp( '#Longitude: <input id="hello-lng" type="text"#', $res );
+		$this->assertRegExp( '#value="1111">#', $res );
+		$this->assertRegExp( '#value="2222">#', $res );
 	}
 }
