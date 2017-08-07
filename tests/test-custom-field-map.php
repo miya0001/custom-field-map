115defd62771ec5e7fd7b0558a6bd78100d5456b
miya0001 <miya@wpist.me>
2017-08-07 21:12:53 +0900

    add some tests to load css and js

--- tests/test-custom-field-map.php
+++ tests/test-custom-field-map.php
@@ -1,12 +1,40 @@
 <?php
 
-class Custom_Field_Map_Tests extends WP_UnitTestCase {
+class Custom_Field_Map_Tests extends WP_UnitTestCase
+{
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public
+	function test_enqueue_fires_on_post()
+	{
+		$map = new Miya\WP\Custom_Field\Map( 'map', 'Map' );
+		$map->add( 'post' );
+
+		convert_to_screen( 'post' );
+		set_current_screen( 'post-new.php' );
+		do_action( 'admin_enqueue_scripts', 'post-new.php' );
+		$this->assertTrue( wp_style_is( 'leaflet' ) );
+		$this->assertTrue( wp_script_is( 'riot' ) );
+		$this->assertTrue( wp_script_is( 'leaflet' ) );
+		$this->assertTrue( wp_script_is( 'app' ) );
+	}
 
 	/**
-	 * A single example test.
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
 	 */
-	function test_class() {
+	function test_enqueue_fires_on_page() {
 		$map = new Miya\WP\Custom_Field\Map( 'map', 'Map' );
-		$map->add( 'map' );
+		$map->add( array( 'page', 'post' ) );
+
+		convert_to_screen( 'page' );
+		set_current_screen( 'post-new.php' );
+		do_action( 'admin_enqueue_scripts', 'post-new.php' );
+		$this->assertTrue( wp_style_is( 'leaflet' ) );
+		$this->assertTrue( wp_script_is( 'riot' ) );
+		$this->assertTrue( wp_script_is( 'leaflet' ) );
+		$this->assertTrue( wp_script_is( 'app' ) );
 	}
 }
