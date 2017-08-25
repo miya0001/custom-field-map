7132f5575149b5456b5789d021bd40af576f4a25
miya0001 <miya@wpist.me>
2017-08-26 06:26:34 +0900

    meta key to be id

--- tests/test-custom-field-map.php
+++ tests/test-custom-field-map.php
@@ -54,8 +54,7 @@ class Custom_Field_Map_Tests extends WP_UnitTestCase
 
 		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
 
-		update_post_meta( $post_id, '_hello-lat', '1111' );
-		update_post_meta( $post_id, '_hello-lng', '2222' );
+		update_post_meta( $post_id, 'hello', array( 'lat' => '1111', 'lng' => 2222 ) );
 
 		$GLOBALS['post'] = get_post( $post_id );
 
@@ -73,5 +72,37 @@ class Custom_Field_Map_Tests extends WP_UnitTestCase
 		$this->assertRegExp( '#id="hello-lng"#', $res );
 		$this->assertRegExp( '#value="1111">#', $res );
 		$this->assertRegExp( '#value="2222">#', $res );
+	}
+
+	/**
+	 * A test `form()` should be called.
+	 */
+	function test_form_should_be_called_with_empty_meta()
+	{
+		global $wp_meta_boxes;
+
+		$test = new Miya\WP\Custom_Field\Map( 'hello', 'Hello' );
+		$test->add( 'my-post-type' );
+
+		do_action( 'add_meta_boxes' );
+
+		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+
+		$GLOBALS['post'] = get_post( $post_id );
+
+		ob_start();
+		$metaboxes = $wp_meta_boxes['my-post-type']['advanced']['default'];
+		call_user_func(
+			array( $metaboxes['hello']['callback'][0], $metaboxes['hello']['callback'][1] ),
+			get_post( $post_id ),
+			array()
+		);
+		$res = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertRegExp( '#id="hello-lat"#', $res );
+		$this->assertRegExp( '#id="hello-lng"#', $res );
+		$this->assertRegExp( '#value="">#', $res );
+		$this->assertRegExp( '#value="">#', $res );
 	}
 }
