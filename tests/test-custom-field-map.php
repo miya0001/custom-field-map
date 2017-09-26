0f9456c8c7002ae7306a219021071c78b29b8147
miya0001 <miya@wpist.me>
2017-09-26 15:44:26 +0900

    update tests

--- tests/test-custom-field-map.php
+++ tests/test-custom-field-map.php
@@ -43,42 +43,6 @@ class Custom_Field_Map_Tests extends WP_UnitTestCase
 	/**
 	 * A test `form()` should be called.
 	 */
-	function test_form_should_be_called()
-	{
-		global $wp_meta_boxes;
-
-		$test = new Miya\WP\Custom_Field\Map( 'hello', 'Hello' );
-		$test->add( 'my-post-type' );
-
-		do_action( 'add_meta_boxes' );
-
-		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
-
-		update_post_meta( $post_id, 'hello', array( 'lat' => '1111', 'lng' => 2222, 'zoom' => 9 ) );
-
-		$GLOBALS['post'] = get_post( $post_id );
-
-		ob_start();
-		$metaboxes = $wp_meta_boxes['my-post-type']['advanced']['default'];
-		call_user_func(
-			array( $metaboxes['hello']['callback'][0], $metaboxes['hello']['callback'][1] ),
-			get_post( $post_id ),
-			array()
-		);
-		$res = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertRegExp( '#id="custom-field-map-lat"#', $res );
-		$this->assertRegExp( '#id="custom-field-map-lng"#', $res );
-		$this->assertRegExp( '#id="custom-field-map-zoom"#', $res );
-		$this->assertRegExp( '#value="1111">#', $res );
-		$this->assertRegExp( '#value="2222">#', $res );
-		$this->assertRegExp( '#value="9">#', $res );
-	}
-
-	/**
-	 * A test `form()` should be called.
-	 */
 	function test_form_should_be_called_with_empty_meta()
 	{
 		global $wp_meta_boxes;
